96215200ebdd76b1f21272cfda0a310b059887d2
Joao Paulo <joaopaulodantasbernardo@gmail.com>
2022-01-25 19:09:00 -0300

    Category

--- src/main/java/com/jpdeveloper/course/config/TestConfig.java
+++ src/main/java/com/jpdeveloper/course/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.jpdeveloper.course.entities.Category;
 import com.jpdeveloper.course.entities.Order;
 import com.jpdeveloper.course.entities.User;
 import com.jpdeveloper.course.entities.enums.OrderStatus;
+import com.jpdeveloper.course.repositories.CategoryRepository;
 import com.jpdeveloper.course.repositories.OrderRepository;
 import com.jpdeveloper.course.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Kooks");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "98888888", "12345");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "9877777777", "123456");
